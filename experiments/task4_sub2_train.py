61652b5c1862196eefab918f0583e649f0e2f355
Rohit Tripathy <rohitkt10@gmail.com>
2021-12-17 18:07:30 -0500

    adding scripts for table 1

---
+++ experiments/task4_sub2_train.py
@@ -0,0 +1,133 @@
+"""
+Task 4 - Comparing the classification performance and interpretability of
+shallow and deep CNN models under various regularization settings.
+-------------------------------------------------------------------
+
+Subtask 4.2 - Table 2 input mixup training results
+
+In this subtask we train the shallow and deep CNN with batch normalization
+dropout. The test is conducted for 3 versions of the shallow and deep
+CNNs each - scaling each of these types of models by a factor of 1, 4 and 8.
+"""
+
+import numpy as np, os, sys, h5py, pandas as pd, argparse
+from pdb import set_trace as keyboard
+
+sys.path.append("..")
+from src.callbacks import ModelInterpretabilityCallback, LRCallback
+from src.model_zoo import deep_cnn, shallow_cnn
+from src.utils import _get_synthetic_data
+from src.models import AugmentedModel
+from src.augmentations import MixupAugmentation
+from train_model_utils import get_keyboard_arguments
+from train_model_utils import (models, BASERESULTSDIR, SYNTHETIC_DATADIR)
+
+import tensorflow as tf
+from tensorflow import keras as tfk
+from tensorflow.keras.callbacks import ModelCheckpoint
+TASKDIR = "task_4_sub_2"
+
+def main():
+    # get keyboard arguments ; defined in the file train_model_utils.py
+    args = get_keyboard_arguments()
+
+    # get data
+    traindata, validdata, testdata, model_test = _get_synthetic_data(SYNTHETIC_DATADIR)
+    traindata = tf.data.Dataset.from_tensor_slices(traindata)
+    validdata = tf.data.Dataset.from_tensor_slices(validdata)
+
+    # set up the checkpoint directory
+    BN = 'bn' if args.bn else 'no_bn'
+    first_act = args.activation
+    RESULTSDIR = os.path.join(BASERESULTSDIR, TASKDIR,f"{BN}", f"{first_act}")
+    CKPTDIR = os.path.join(
+                        RESULTSDIR,
+                        f"{args.type.lower()}_{args.factor}",
+                         )
+    
+    # do several trials
+    for trial in range(args.start_trial, args.end_trial+1):
+
+        # instantiate model
+        get_model = models[args.type.lower()].get_model
+        model = get_model(
+                    input_shape=(200,4),
+                    num_outputs=1,
+                    bn=args.bn,
+                    dropout1=0.1,
+                    dropout2=0.5,
+                    kernel_regularizer=None,
+                    activation=first_act,
+                    name="model",
+                    factor=args.factor,
+                    logits_only=False,
+                        )
+        augmentation = MixupAugmentation(alpha=0.2)
+        model = AugmentedModel(model=model, augmentations=[augmentation])
+    
+        # set up compile options and compile the model
+        acc = tfk.metrics.BinaryAccuracy(name='acc')
+        auroc = tfk.metrics.AUC(curve='ROC', name='auroc')
+        aupr = tfk.metrics.AUC(curve='PR', name='aupr')
+        optimizer = tfk.optimizers.Adam(learning_rate=1e-3)
+        loss = tfk.losses.BinaryCrossentropy(from_logits=False, label_smoothing=0.0)
+        model.compile(optimizer=optimizer, loss=loss, metrics=[acc, auroc, aupr], )
+
+        # set up callbacks
+        callbacks = []
+        if args.es:
+            es_callback = tfk.callbacks.EarlyStopping(
+                                            monitor='val_auroc',
+                                            patience=10,
+                                            verbose=1,
+                                            mode='max',
+                                            restore_best_weights=True
+                                                    )
+            callbacks.append(es_callback)
+        if args.reduce_lr:
+            reduce_lr = tfk.callbacks.ReduceLROnPlateau(
+                                            monitor='val_auroc',
+                                            factor=0.2,
+                                            patience=4,
+                                            min_lr=1e-7,
+                                            mode='max',
+                                            verbose=1
+                                                )
+            callbacks.append(reduce_lr)
+
+        ## directory to save results from current trial
+        ckptdir = os.path.join(CKPTDIR, f"trial_{trial:02d}")
+        if not os.path.exists(ckptdir):
+            os.makedirs(ckptdir)
+        csvlogger = tfk.callbacks.CSVLogger(os.path.join(ckptdir, "log.csv"))
+        ckpt_callback = tfk.callbacks.ModelCheckpoint(
+                                        filepath=os.path.join(ckptdir, "ckpt_epoch-{epoch:04d}"),
+                                        monitor="val_auroc",
+                                        verbose=1,
+                                        mode='max',
+                                        save_best_only=True,
+                                        save_weights_only=False,
+                                        save_freq="epoch",
+                                            )
+        interp_callback = ModelInterpretabilityCallback(
+                                            testdata=testdata,
+                                            model_test=model_test,
+                                            ckptdir=ckptdir,
+                                            track_saliency=True,
+                                            track_sg=True,
+                                            track_intgrad=True,
+                                                )
+        callbacks = callbacks + [csvlogger, ckpt_callback, interp_callback]
+
+        # fit the model
+        model.fit(
+            traindata.shuffle(10000).batch(128),
+            validation_data=validdata.batch(64),
+            epochs=100,
+            callbacks=callbacks,
+                )
+
+if __name__ == '__main__':
+    main()
+        
+        

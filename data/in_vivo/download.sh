e04280e626ea14d9005c5bafbf08346d23677bab
Rohit Tripathy <rohitkt10@gmail.com>
2022-04-12 13:36:47 -0400

    adding in vivo utils

---
+++ data/in_vivo/download.sh
@@ -0,0 +1,3 @@
+wget https://www.dropbox.com/s/e972vdcwsvhuoca/GM12878.h5
+wget https://www.dropbox.com/s/no2x1tfzo1r4pb2/A549.h5
+wget https://www.dropbox.com/s/01t5aiyvrm0avca/HeLa-S3.h5
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np, os, h5py
+IN_VIVO_NAMES = ['A549', 'GM12878', 'HeLa-S3']
 
 def _get_synthetic_data(datadir):
     """
@@ -19,3 +20,18 @@ def _get_synthetic_data(datadir):
     x_valid = x_valid.transpose([0,2,1])
     x_test = x_test.transpose([0,2,1])
     return (x_train, y_train), (x_valid, y_valid), (x_test, y_test), model_test
+
+
+def get_invivo_data(datadir, name='A549'):
+    filepath = os.path.join(datadir, f'{name}.h5')
+    assert os.path.exists(filepath), 'No such file available.'
+
+    with h5py.File(filepath, 'r') as dataset:
+        x_train = np.array(dataset['x_train']).astype(np.float32)
+        y_train = np.array(dataset['y_train']).astype(np.int32)
+        x_valid = np.array(dataset['x_valid']).astype(np.float32)
+        y_valid = np.array(dataset['y_valid']).astype(np.int32)
+        x_test = np.array(dataset['x_test']).astype(np.float32)
+        y_test = np.array(dataset['y_test']).astype(np.int32)
+    
+    return (x_train, y_train), (x_valid, y_valid), (x_test, y_test)
